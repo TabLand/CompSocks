20f181836c6d9766a22454bb641574a758dd1d93
Ijtaba Hussain <ijtaba.hussain.1@city.ac.uk>
2013-03-06 06:55:15 +0000

    cool js

--- Script.js
+++ Script.js
@@ -1,3 +1,6 @@
+//change this to initial text..
+var str = null;
+var printerBusy = false;
 function serverComms(str)
 {
 	if (str.length==0){ 
@@ -15,10 +18,30 @@ function serverComms(str)
 	}
 	xmlhttp.onreadystatechange=function(){
 	  if (xmlhttp.readyState==4 && xmlhttp.status==200){
-	       document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+	  	//this is the function which uses xmlhttp.responseText to use responses from server
+	  	//change this to cute printer
+	       document.getElementById("console").innerHTML=xmlhttp.responseText;
 	     }
 	  }
-}
 	xmlhttp.open("GET","comms.php?q="+str,true);
 	xmlhttp.send();
+}
+
+function cutePrinter(appendMode){
+	if(str.length > 0){
+		  printerBusy = true;
+		if(appendMode){
+		  document.getElementById("console").innerHTML += str.substr(0,1);
+  		  str = str.substr(1,str.length-1);
+		  setTimeout("cutePrinter("+appendMode+")",timeout);	  
+		}
+		else{
+		  document.getElementById("console").innerHTML = str.substr(0,1);
+  		  str = str.substr(1,str.length-1);
+  		  setTimeout("cutePrinter("+true+")",timeout);
+		}
+	}
+	else{
+ 		printerBusy = false;
+	}
 }

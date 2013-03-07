90b4b2f56d45c07498800b9ae7234f5f6767445e
Ijtaba Hussain <ijtaba.hussain.1@city.ac.uk>
2013-03-07 09:21:38 +0000

    a lot of changes. testing printer

--- Script.js
+++ Script.js
@@ -1,9 +1,13 @@
 //change this to initial text..
 var str = null;
 var clrstr = null;
-var printerBusy = false;
 var xmlhttp = null;
+var timeout = 10;
+var printing = new Array();
+
 var url = "http://localhost/rbbc526/comms.php";
+var debugQueue = new Array();
+var consoleQueue = new Array();
 function serverComms()
 {
 	if (clrstr.length==0){ 
@@ -28,36 +32,68 @@ function serverComms()
 }
 
 function httpHandler(){
-	// all statuses here can be printed to the screen.
-	  debug("http state = " + xmlhttp.readyState);
-  	  debug("http status = " + xmlhttp.readyState);
-  	  debug("http response = " + xmlhttp.response);
+
+	  //bug
+  	  if(xmlhttp.response.length < 1){
+  	  	response = "No valid response";
+  	  }
+  	  else {
+  	  	response = xmlhttp.response;
+  	  }
+	// all statuses here can be printed to the debug screen.
+	  debug("------------------------------------------------------\nStart Packet");
+	  debug("http{state=" + xmlhttp.readyState + ",status=" + xmlhttp.readyState + "}");
+  	  debug("http.response = " + response);
+  	  
 	  if (xmlhttp.readyState==4 && xmlhttp.status==200){
 	  	//this is the function which uses xmlhttp.responseText to use responses from server
 	  	//change this to cute printer
 	       str = xmlhttp.responseText;
-	       debug("Cute printer typing<br/>" + str);
+	       debug("Cute printer typing: " + str);
 	       //more debugging
 	     }
-
+	debug("End Packet");
 }
 
-function cutePrinter(appendMode){
-	if(str.length > 0){
-		  printerBusy = true;
-		if(appendMode){
-		  document.getElementById("console").innerHTML += str.substr(0,1);
-  		  str = str.substr(1,str.length-1);
-		  setTimeout("cutePrinter("+appendMode+")",timeout);	  
+function cutePrinter(appendMode, id, queueName){
+	print_ing = printing.indexOf(id);
+	queue = eval(queueName);
+	//threading function
+	func = "cutePrinter("+appendMode+",\""+id+"\",\""+queueName+"\")"
+	if(queue.length > 0) {
+		//read from start, dont modify queue
+		string = queue[0];
+		//if id is not being printed to, set it as printing
+		if(print_ing==-1){
+			printing.push(id);
+		}
+		if(string.length > 0){
+			if(appendMode){
+			  document.getElementById(id).innerHTML += string.substr(0,1);
+  			  string = string.substr(1,string.length-1);
+  			  eval(queueName)[0] = string;
+
+			  setTimeout(func,timeout);	
+		          //alert(func);
+			}
+			else{
+			  document.getElementById(id).innerHTML = string.substr(0,1);
+  			  string = string.substr(1,string.length-1);
+  			  eval(queueName)[0] = string;
+			  setTimeout(func,timeout);	
+			}
 		}
 		else{
-		  document.getElementById("console").innerHTML = str.substr(0,1);
-  		  str = str.substr(1,str.length-1);
-  		  setTimeout("cutePrinter("+true+")",timeout);
+			  //string is empty, modify queue
+  			  eval(queueName).shift();
+    			  setTimeout(func,timeout);
 		}
 	}
 	else{
- 		printerBusy = false;
+		//stop printing if printing
+		if(print_ing!=-1){
+			printing.splice(prin_ting,1);
+		}
 	}
 }
 //clr submit
@@ -80,7 +116,11 @@ function submit(keyPress){
 
 }
 function debug(dump){
-	       alert(unescapeHTML(escapeHTML(dump)));
+	       debugDiv = document.getElementById("debug");
+	       //push at end
+	       debugQueue.push(dump + "<br/>");
+	       if(printing.indexOf("debug")==-1) cutePrinter(true,"debug","debugQueue");
+	       debugDiv.scrollTop=debugDiv.scrollHeight;
 }
 
 function escapeHTML(html){
@@ -90,10 +130,10 @@ function unescapeHTML(html){
 	return html.replace("\n","<br/>","g").replace( "&amp;","&","g").replace("&lt;","<","g").replace("&gt;",">","g");
 }
 function focus(inp){
-	alert(inp.value);
 	inp.select();
 	inp.focus();
 }
 function focusSet(){
 	focus(document.getElementById("clr"));
 }
+
